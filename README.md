a1ee1b7fcd479f5da9e8014d2cb80395ea43d667
Cambridge Spark <tech+cspark_account@cambridgespark.com>
2019-07-25 15:31:14 +0000

    Update repository

---
+++ README.md
@@ -0,0 +1,12 @@
+# Big Data
+
+In this project, you will be using the Spark RDD API for data processing.
+
+## Spark RDD: HackerNews data analysis challenge
+
+You will analyse a dataset of (almost) all submitted HackerNews posts. 
+
+1) First run `python get_data.py` to download the data. Alternatively you can download it from https://s3-eu-west-1.amazonaws.com/kate-datasets/hackernews/HNStories.zip and unzip it.
+
+2) Then you can use the notebook `spark-rdd-homework.ipynb` to prototype your functions
+3) Finally write down your functions in `spark_rdd.py` and submit on K.A.T.E.
---
+++ spark_rdd.py
@@ -0,0 +1,144 @@
+def count_elements_in_dataset(dataset):
+    """
+    Given a dataset loaded on Spark, return the
+    number of elements.
+    :param dataset: dataset loaded in Spark context
+    :type dataset: a Spark RDD
+    :return: number of elements in the RDD
+    """
+
+    raise NotImplementedError
+
+
+def get_first_element(dataset):
+    """
+    Given a dataset loaded on Spark, return the
+    first element
+    :param dataset: dataset loaded in Spark context
+    :type dataset: a Spark RDD
+    :return: the first element of the RDD
+    """
+
+    raise NotImplementedError
+
+
+def get_all_attributes(dataset):
+    """
+    Each element is a dictionary of attributes and their values for a post.
+    Can you find the set of all attributes used throughout the RDD?
+    The function dictionary.keys() gives you the list of attributes of a dictionary.
+    :param dataset: dataset loaded in Spark context
+    :type dataset: a Spark RDD
+    :return: all unique attributes collected in a list
+    """
+
+    raise NotImplementedError
+
+
+def get_elements_w_same_attributes(dataset):
+    """
+    We see that there are more attributes than just the one used in the first element.
+    This function should return all elements that have the same attributes
+    as the first element.
+
+    :param dataset: dataset loaded in Spark context
+    :type dataset: a Spark RDD
+    :return: an RDD containing only elements with same attributes as the
+    first element
+    """
+
+    raise NotImplementedError
+
+
+def get_min_max_timestamps(dataset):
+    """
+    Find the minimum and maximum timestamp in the dataset
+    :param dataset: dataset loaded in Spark context
+    :type dataset: a Spark RDD
+    :return: min and max timestamp in a tuple object
+    :rtype: tuple
+    """
+
+    raise NotImplementedError
+
+
+def get_number_of_posts_per_bucket(dataset, min_time, max_time):
+    """
+    Using the `get_bucket` function defined in the notebook (redefine it in this file), this function should return a
+    new RDD that contains the number of elements that fall within each bucket.
+    :param dataset: dataset loaded in Spark context
+    :type dataset: a Spark RDD
+    :param min_time: Minimum time to consider for buckets (datetime format)
+    :param max_time: Maximum time to consider for buckets (datetime format)
+    :return: an RDD with number of elements per bucket
+    """
+
+    raise NotImplementedError
+
+
+def get_number_of_posts_per_hour(dataset):
+    """
+    Using the `get_hour` function defined in the notebook (redefine it in this file), this function should return a
+    new RDD that contains the number of elements per hour.
+    :param dataset: dataset loaded in Spark context
+    :type dataset: a Spark RDD
+    :return: an RDD with number of elements per hour
+    """
+
+    raise NotImplementedError
+
+
+def get_score_per_hour(dataset):
+    """
+    The number of points scored by a post is under the attribute `points`.
+    Use it to compute the average score received by submissions for each hour.
+    :param dataset: dataset loaded in Spark context
+    :type dataset: a Spark RDD
+    :return: an RDD with average score per hour
+    """
+
+    raise NotImplementedError
+
+
+def get_proportion_of_scores(dataset):
+    """
+    It may be more useful to look at sucessful posts that get over 200 points.
+    Find the proportion of posts that get above 200 points per hour.
+    :param dataset: dataset loaded in Spark context
+    :type dataset: a Spark RDD
+    :return: an RDD with the proportion of scores over 200 per hour
+    """
+
+    raise NotImplementedError
+
+
+def get_proportion_of_success(dataset):
+    """
+    Using the `get_words` function defined in the notebook to count the
+    number of words in the title of each post, look at the proportion
+    of successful posts for each title length.
+
+    Note: If an entry in the dataset does not have a title, it should
+    be counted as a length of 0.
+
+    :param dataset: dataset loaded in Spark context
+    :type dataset: a Spark RDD
+    :return: an RDD with the proportion of successful post per title length
+    """
+
+    raise NotImplementedError
+
+
+def get_title_length_distribution(dataset):
+    """
+    Count for each title length the number of submissions with that length.
+
+    Note: If an entry in the dataset does not have a title, it should
+    be counted as a length of 0.
+
+    :param dataset: dataset loaded in Spark context
+    :type dataset: a Spark RDD
+    :return: an RDD with the number of submissions per title length
+    """
+
+    raise NotImplementedError
