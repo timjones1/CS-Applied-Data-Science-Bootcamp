a7aca0daa3683c29daef87a3fc1f13db0c01148b
timjones101 <biggafish8@gmail.com>
2020-03-25 22:29:16 +0000

    123

--- model.py
+++ model.py
@@ -1,4 +1,4 @@
-#imports
+# imports
 import pandas as pd
 import numpy as np
 import json
@@ -45,27 +45,27 @@ def preprocess(data):
     :type df: pd.DataFrame
     :return: X, y, X_eval
     """
-    data = raw_data[~raw_data.state.isnull()]
-    data.blurb.fillna("missing",inplace=True)
-    data.name.fillna("missing",inplace=True)
 
-    cat_get_slug = lambda x : json.loads(x).get('slug')
-    data['cat_slug'] = raw_data['category'].apply(cat_get_slug)
+    data.blurb.fillna("missing", inplace=True)
+    data.name.fillna("missing", inplace=True)
 
-    cat_get_parent = lambda x : json.loads(x).get('parent_id')
-    data['cat_parent'] = data['category'].apply(cat_get_parent)
-    data['goal'] = np.log((data.goal*data.static_usd_rate)+1)
-    categorical_cols = ['cat_slug','country']
+    data['cat_slug'] = data['category'].apply(
+        lambda x: json.loads(x).get('slug'))
+
+    data['cat_parent'] = data['category'].apply(
+        lambda x: json.loads(x).get('parent_id'))
+
+    data['goal'] = np.log1p((data.goal*data.static_usd_rate))
 
     data['blurb_length'] = data['blurb'].apply(len)
     data['name_length'] = data['name'].apply(len)
 
-    #convert time dependent fields into datetime and add extra fields day,month,year
+    # convert time dependent fields into datetime and add extra date fields
     from datetime import datetime
-    utc_time = lambda ts:datetime.utcfromtimestamp(ts)
+    utc_time = lambda ts: datetime.utcfromtimestamp(ts)
 
-    for col in ["launched_at","deadline",'created_at']:
-        data[col] = raw_data[col].apply(utc_time)
+    for col in ['launched_at', 'deadline', 'created_at']:
+        data[col] = data[col].apply(utc_time)
         data[f'{col}_day'] = pd.DatetimeIndex(data[col]).day
         data[f'{col}_month'] = pd.DatetimeIndex(data[col]).month
         data[f'{col}_year'] = pd.DatetimeIndex(data[col]).year
@@ -76,22 +76,26 @@ def preprocess(data):
     data['loc_name'] = data['location'].apply(
         lambda x: json.loads(x).get('name') if (np.all(pd.notnull(x))) else "Other")
     # get only states with > 5 examples and names > 50 examples
-    top_names = data['loc_name'].value_counts()[data['loc_name'].value_counts()>10]
-    top_states = data['loc_state'].value_counts()[data['loc_state'].value_counts()>2]
+    top_names = data['loc_name'].value_counts()[
+        data['loc_name'].value_counts() > 10]
+    top_states = data['loc_state'].value_counts()[
+        data['loc_state'].value_counts() > 2]
 
-    data['loc_name'] = data['loc_name'].apply(lambda n: n if n in top_names else "other")
-    data['loc_state'] = data['loc_state'].apply(lambda n: n if n in top_states else "other")
+    data['loc_name'] = data['loc_name'].apply(
+        lambda n: n if n in top_names else "other")
+    data['loc_state'] = data['loc_state'].apply(
+        lambda n: n if n in top_states else "other")
 
     # Calculates campaign length, thanks to @paulo
     data['campaign_active_length'] = pd.to_timedelta(data['deadline'] - data['launched_at'], unit='s').dt.days
     data['campaign_total_length'] = pd.to_timedelta(data['deadline'] - data['created_at'], unit='s').dt.days
     data['campaign_prep_length'] = pd.to_timedelta(data['launched_at'] - data['created_at'], unit='s').dt.days
-    
-     msk_eval = df.evaluation_set
+
+    msk_eval = data.evaluation_set
 
     X = data[~msk_eval].drop(["state"], axis=1)
     y = data[~msk_eval]["state"]
-    X_eval = df[msk_eval].drop(["state"], axis=1)
+    X_eval = data[msk_eval].drop(["state"], axis=1)
 
     return X, y, X_eval
 
@@ -107,8 +111,8 @@ def train(X, y):
     """
 
     numeric_features = ['goal', 'launched_at_day', 'launched_at_month',
-                        'launched_at_year', 'deadline_day',"deadline_month",
-                        'deadline_year', 'created_at_day','created_at_month',
+                        'launched_at_year', 'deadline_day', "deadline_month",
+                        'deadline_year', 'created_at_day', 'created_at_month',
                         'created_at_year', 'campaign_active_length',
                         'campaign_total_length', 'campaign_prep_length']
     numeric_transformer = Pipeline(steps=[
@@ -117,49 +121,49 @@ def train(X, y):
 
     text_features = 'blurb'
     text_transformer = Pipeline([
-        ('vect', CountVectorizer(ngram_range=(1,3))),
+        ('vect', CountVectorizer(ngram_range=(1, 3))),
         ('tfidf', TfidfTransformer(use_idf=True)),
     ])
 
     name_features = 'name'
     name_transformer = Pipeline([
-        ('vect_n', CountVectorizer(ngram_range=(1,3))),
+        ('vect_n', CountVectorizer(ngram_range=(1, 3))),
         ('tfidf_n', TfidfTransformer(use_idf=True)),
     ])
 
-    categorical_features = ['country','cat_slug','loc_name','loc_state'] #removed currency
+    categorical_features = ['country','cat_slug','loc_name','loc_state']
     categorical_transformer = Pipeline(steps=[
         ('imputer', SimpleImputer(strategy='constant', fill_value='missing')),
         ('onehot', OneHotEncoder(handle_unknown='ignore')),
     ])
-
 
     preprocessor = ColumnTransformer(
         transformers=[
             ('num', numeric_transformer, numeric_features),
             ('cat', categorical_transformer, categorical_features),
             ('text_blurb', text_transformer, text_features),
-            ('name_blurb', text_transformer, name_features)
+            ('name_blurb', name_transformer, name_features)
         ])
 
     # Append classifier to preprocessing pipeline.
     # Now we have a full prediction pipeline.
-    model = Pipeline(steps=[('preprocessor', preprocessor),
-                          ('sgd',SGDClassifier(alpha=6e-05, average=False, class_weight=None,
-                                   early_stopping=False, epsilon=0.1, eta0=0.0,
-                                   fit_intercept=True, l1_ratio=0.15,
-                                   learning_rate='optimal', loss='hinge',
-                                   max_iter=140, n_iter_no_change=5, n_jobs=None,
-                                   penalty='l2', power_t=0.5, random_state=None,
-                                   shuffle=True, tol=0.001, validation_fraction=0.1,
-                                   verbose=0, warm_start=False))]) #('svc', LinearSVC(dual=False) 74.4
-
-    model.fit(X_train, y_train)
+    model = Pipeline(
+        steps=[('preprocessor', preprocessor),
+               ('sgd', SGDClassifier(
+                    alpha=6e-05, average=False, class_weight=None,
+                    early_stopping=False, epsilon=0.1, eta0=0.0,
+                    fit_intercept=True, l1_ratio=0.15,
+                    learning_rate='optimal', loss='hinge',
+                    max_iter=140, n_iter_no_change=5, n_jobs=None,
+                    penalty='l2', power_t=0.5, random_state=None,
+                    shuffle=True, tol=0.001, validation_fraction=0.1,
+                    verbose=0, warm_start=False))])
+    model.fit(X, y)
     return model
 
 
 def predict(model, X_test):
-    """This functions takes your trained model as well 
+    """This functions takes your trained model as well
     as a processed test dataset and returns predictions.
 
     On KATE, the processed test dataset will be the X_eval you built
