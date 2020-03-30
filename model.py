d2cec5d7e1cb2f4976643db131ac3d18303bb109
timjones101 <biggafish8@gmail.com>
2020-03-30 15:41:16 +0100

    12

--- model.py
+++ model.py
@@ -182,7 +182,7 @@ def train(X, y):
                                 colsample_bytree=0.3,
                                 subsample=0.8,
                                 objective='binary:logistic',
-                                n_estimators=245,
+                                n_estimators=250,
                                 reg_alpha=0.3,
                                 max_depth=5,
                                 gamma=10))])
