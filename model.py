0dbd9789bd732419656005015d85395c91907a5f
timjones101 <biggafish8@gmail.com>
2020-05-26 16:59:23 +0100

    123

--- model.py
+++ model.py
@@ -1,3 +1,27 @@
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.pipeline import Pipeline
+from sklearn.tree import DecisionTreeClassifier
+
+
+class Processor(BaseEstimator, TransformerMixin):
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X, y=None):
+        cols = ["Product_Info_4"]
+
+        if y is None:
+            return X[cols]
+
+        return X[cols], y
+
+
+def build_model():
+    preprocessor = Processor()
+    model = DecisionTreeClassifier()
+    return Pipeline([("preprocessor", preprocessor), ("model", model)])
+
+'''
 def build_model():
     """This function builds a new model and returns it.
 
@@ -10,3 +34,4 @@ def build_model():
     :return: a new instance of your model
     """
     raise NotImplementedError
+'''
