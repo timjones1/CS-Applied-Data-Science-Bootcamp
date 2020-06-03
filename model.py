4305d0ebf2840c98128525d61293b4441ba7e575
timjones101 <biggafish8@gmail.com>
2020-06-03 22:09:15 +0100

    123

--- model.py
+++ model.py
@@ -3,23 +3,43 @@ from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.compose import ColumnTransformer
+
 import lightgbm as lgb
 
-cat_cols = ["Product_Info_2"]
-cat_pipe = Pipeline([('onehot', OneHotEncoder(handle_unknown='ignore'))])
 
-preprocessor = ColumnTransformer(
-	[('cat', cat_pipe, cat_cols)],remainder='passthrough')
+class Processor(BaseEstimator, TransformerMixin):
+
+	
+	def fit(self, X, y=None):
+		
+		cat_cols = ["Product_Info_2"]
+		
+		cat_pipe = Pipeline([('onehot', OneHotEncoder(handle_unknown='ignore'))])
+
+		self.preprocessor = ColumnTransformer(
+			[('cat', cat_pipe, cat_cols)],
+			remainder='passthrough')
+
+		self.preprocessor.fit(X, y)   
+
+ 		return self
+
+	def transform(self, X, y=None):
+
+		if y is None:
+			return self.preprocessor.transform(X)
+
+		return preprocessor.transform(X), y[:, 1]
 
 
 def build_model():
 
-    return Pipeline([
-		("preprocessor", preprocessor),
-        ("model", lgb.LGBMClassifier(
-         	num_leaves=45,
+	return Pipeline([
+		("preprocessor", Processor()),
+		("model", lgb.LGBMClassifier(
+			num_leaves=45,
 			learning_rate=0.04,
 			n_estimators=300,
 			min_data_in_leaf=100,
 			class_weights="balanced"))
-    ])
+	])
