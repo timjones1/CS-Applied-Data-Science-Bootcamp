18efc270848529e6c01882d3e24ad66587fc125b
timjones101 <biggafish8@gmail.com>
2020-06-03 21:30:22 +0100

    linting

--- model.py
+++ model.py
@@ -9,33 +9,33 @@ import lightgbm as lgb
 
 class Processor(BaseEstimator, TransformerMixin):
     def fit(self, X, y=None):
-        
+
         cat_cols = ["Product_Info_2"]
         cat_pipe = Pipeline([('onehot', OneHotEncoder(handle_unknown='ignore'))])
-    
+
         self.preprocessor = ColumnTransformer(
             [('cat', cat_pipe, cat_cols)],
             remainder='passthrough')
-        
-        self.preprocessor.fit(X,y)        
+
+        self.preprocessor.fit(X, y)   
 
         return self
 
     def transform(self, X, y=None):
-        
+
         if y is None:
             return self.preprocessor.transform(X)
 
-        return preprocessor.transform(X), y[:,1]
-    
+        return preprocessor.transform(X), y[:, 1]
+
 
 def build_model():
-    
-    return Pipeline([("preprocessor", Processor()), 
-        ("model", lgb.LGBMClassifier(
-         num_leaves=45,
-         learning_rate=0.04,
-         n_estimators=300,
-         min_data_in_leaf=100,
-         class_weights="balanced"))
-    ])
+
+    return Pipeline([("preprocessor", Processor()),
+        			 ("model", lgb.LGBMClassifier(
+         				num_leaves=45,
+         				learning_rate=0.04,
+         				n_estimators=300,
+         				min_data_in_leaf=100,
+         				class_weights="balanced"))
+    				])
